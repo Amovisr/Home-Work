8153ee5fd622a3fb3d473ebef67aeec0b5ce77cb
Armen <alldayend@yandex.ru>
2022-01-19 21:19:17 +0300

    Выполненная домашняя работа №1

---
+++ Домашнее задание №1/Домашнее задание №1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Домашнее задание 1 урока 
+Console.Write("Как вас зовут ? ");
+string name = Console.ReadLine();
+Console.WriteLine("Привет " + name + ", " + "сегодняшняя дата " + DateTime.Now);
+
+/* 
+ * Console.WriteLine($"Привет {name}, сегодняшеяя дата [{DateTime.Now}] "); с интрополяцией 
+*/
