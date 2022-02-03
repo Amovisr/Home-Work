aaeae355042c6de7a26241d758e5bda6fdead7c7
Armen <alldayend@yandex.ru>
2022-02-04 02:58:56 +0300

    Выполненная домашняя работа №3

---
+++ Домашнее задание №3/Домашнее Задание 3.1/Домашнее Задание 3.1/Program.cs
@@ -0,0 +1,16 @@
+﻿int[,] matrix = new int[5, 5];
+matrix[0, 0] = 1;
+matrix[1, 1] = 1;
+matrix[2, 2] = 1;
+matrix[3, 3] = 1;
+matrix[4, 4] = 1;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        System.Console.Write($"{matrix[j, i]} ");
+    }
+    System.Console.WriteLine();
+}
+
---
+++ Домашнее задание №3/Домашнее Задание 3.4/Домашнее Задание 3.4/Program.cs
@@ -0,0 +1,23 @@
+﻿char[,] SeaBattle = { 
+    { 'X', 'O', 'X', 'X', 'O', 'O', 'O', 'O', 'O', 'O', },
+    { 'X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', },
+    { 'X', 'O', 'O', 'O', 'X', 'X', 'X', 'O', 'O', 'O', },
+    { 'X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', },
+    { 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', },
+    { 'X', 'O', 'O', 'O', 'X', 'X', 'X', 'O', 'O', 'X', },
+    { 'X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', },
+    { 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', },
+    { 'X', 'O', 'O', 'X', 'X', 'O', 'O', 'O', 'O', 'O', },
+    { 'X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', } 
+};
+
+for (int i = 0; i < SeaBattle.GetLength(0); i++)
+{
+    for (int j = 0; j < SeaBattle.GetLength(1); j++)
+    {
+      
+        System.Console.Write($"{SeaBattle[i, j]} ");
+    }
+
+    System.Console.WriteLine();
+}
