2beaa90780db279e595dd184cbb06523c5a6d893
Armen <alldayend@yandex.ru>
2022-02-19 17:55:48 +0300

    Домашнее Задание 6

---
+++ Домашнее задание №6/Домашнее Задание 6/Домашнее Задание 6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+Process[] process = Process.GetProcesses();
+
+for (int i = 0; i < process.Length; i++)
+
+{
+    Process current = process[i];
+
+    Console.WriteLine($"{current.Id}\t{current.ProcessName}\t{current.PeakVirtualMemorySize64}");
+
+   
+}
+
+Console.WriteLine("Укажите просецц который хотите закрыть");
+
+int executProcess = Int32.Parse(Console.ReadLine());
+
+for (int i = 0;  i < process.Length; i++)
+{
+    Process current = process[i];
+
+    if (current.Id == executProcess)
+    {
+        current.Kill();
+        Console.WriteLine($"Данный процесс: ({current.Id}), был завершен");
+    }
+
+}
+
+return 0;
