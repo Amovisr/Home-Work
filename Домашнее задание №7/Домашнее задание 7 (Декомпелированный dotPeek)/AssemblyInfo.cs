ee978ceee5df2fc6b7ddc6be858a26891f98db98
Armen <alldayend@yandex.ru>
2022-02-23 13:25:18 +0300

    Домашнее задание 7

---
+++ Домашнее задание №7/Домашнее задание 7 (Декомпелированный dotPeek)/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Домашнее задание 7")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("Домашнее задание 7")]
+[assembly: AssemblyTitle("Домашнее задание 7")]
+[assembly: AssemblyVersion("1.0.0.0")]
