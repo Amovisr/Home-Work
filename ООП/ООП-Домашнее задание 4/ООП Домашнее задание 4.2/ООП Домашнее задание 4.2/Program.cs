5fe92618aea537201f0a6bced3a9fee7373cdcb4
Armen <alldayend@yandex.ru>
2022-03-31 21:49:43 +0300

    Почти выполненная Дз 4
    (((

---
+++ ООП/ООП-Домашнее задание 4/ООП Домашнее задание 4.2/ООП Домашнее задание 4.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using ООП_Домашнее_задание_4._2.Building;
+using ООП_Домашнее_задание_4._2.BuildingFactory;
+class Program
+{
+    static void Main()
+    {
+        string building = Console.ReadLine();
+
+        BuildingFactory factory = GetFactory(building);
+
+        IDev membership = factory.GetBuilding();
+
+        Console.WriteLine("\n> Building you've just created: \n");
+
+        Console.WriteLine(
+            $"\tBuilding Number:\t\t{membership.BuildingNumber}\n" +
+            $"\tBuilding Height:\t{membership.Height}\n" +
+            $"\tBuilding Floors:\t{membership.Floor}\n");
+
+
+        Console.ReadLine();
+    }
+
+    private static BuildingFactory GetFactory(string membershipType) =>
+     membershipType.ToLower() switch
+     {
+         "g" => new CreateBuilding(6, 7, 1),
+         _ => null
+     };
+}
+
+
+//    Console.WriteLine($"  Дом номер:{building2.buildingNumber} \n  Высота Здания: {building2.Height} метров " +
+//        $"\n  Количество этажей: {building2.floors} \n  Количество квартир на подъезд:{building2.apartments}  \n  Количество квартир на этаж {building2.flat}");
