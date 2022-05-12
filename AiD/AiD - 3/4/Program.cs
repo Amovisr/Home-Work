3880ef4f9ca2f8aacf240d7304ff8c500edee69e
Armen <alldayend@yandex.ru>
2022-05-12 14:00:26 +0300

    Алгоритмы 3 домашка

---
+++ AiD/AiD - 3/4/Program.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+public class StructTest
+{
+    PointStructHomeWork pointOne = new PointStructHomeWork(3, 8);
+    PointStructHomeWork pointTwo = new PointStructHomeWork(4, 21);
+    public static float PointDistance4HomeWork(PointStructHomeWork pointOne, PointStructHomeWork pointTwo)
+    {
+        float x = pointOne.X - pointTwo.X;
+        float y = pointOne.Y - pointTwo.Y;
+        return (x * x) + (y * y); 
+    }
+
+
+    [Benchmark]
+    public void BenchmarkFourthHomerWork()
+    {
+        PointDistance4HomeWork(pointOne, pointTwo);
+    }
+}
+public struct PointStructHomeWork
+{
+    public float X;
+    public float Y;
+    public PointStructHomeWork(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+}
