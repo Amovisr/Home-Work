9c247a9ee2ec0d0c510e8d39163265521cdbf53f
Armen <alldayend@yandex.ru>
2022-05-27 18:42:44 +0300

    Задание 4-5 Алгоритмы

---
+++ AiD/AiD -  4-5/1 задание/Program.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Attributes;
+
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+
+var bechmarkTest = new BechmarkTest();
+bechmarkTest.GenerateNumberts();
+bechmarkTest.TryToFindNumber();    
+public class BechmarkTest
+{
+    string[] someStrings = new string[10001];
+    string stringsToFind = "9956";
+    
+    
+    public void GenerateNumberts()
+    {
+        for (int i = 0; i < someStrings.Length; i++)
+        {
+            someStrings[i] = Convert.ToString(i);
+        }
+
+    }
+
+    public void TryToFindNumber()
+    {
+            if (someStrings.Contains(stringsToFind))
+            {
+                Console.WriteLine($"FIND {stringsToFind} in Array");
+            }
+    }
+
+    [Benchmark]
+    public string BechmarkCheckArray()
+    {
+        TryToFindNumber();
+        return stringsToFind;
+    }
+
+}
+
+
+
+
+
+
+
